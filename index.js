b72643a4fbf06b25db72a6e3e301031d70b77eab
Lauren Omelchenko <lauren@Laurens-MacBook-Pro.local>
2017-01-17 09:56:09 -0500

    bot searches specific airport

--- index.js
+++ index.js
@@ -26,38 +26,46 @@ app.get('/', function(req, res) {
     console.log('auth key: ', process.env.AUTH_KEY);
 
     request(options, function(error, response, body) {
-      console.log(body);
-            if (!error && response.statusCode == 200) {
-                var data = JSON.parse(body);
-                res.send(data);
-            }
+        console.log(body);
+        if (!error && response.statusCode == 200) {
+            var data = JSON.parse(body);
+            res.send(data);
+        }
 
-        });
+    });
 });
 
 app.post('/post', function(req, res) {
-  var options = {
-      url: 'https://xap.ix-io.net/api/v1/my_project_n1OWkqND/airports/DCA',
-      headers: {
-          'Authorization': process.env.AUTH_KEY,
-          'Accept': 'application/json'
-      }
-  };
+    var options = {
+        url: 'https://xap.ix-io.net/api/v1/my_project_n1OWkqND/airports/DCA'+req.body.text,
+        headers: {
+            'Authorization': process.env.AUTH_KEY,
+            'Accept': 'application/json'
+        }
+        //just added this below - havent tested
+        // query: {
+        //     q: req.body.text
+        // }
+    };
 
     request(options, function(error, response, body) {
-        // if (!error && response.statusCode == 200) {
-        //     var data = JSON.parse(body);
-        //     // var first_url = data.response.hits[0].result.url;
-        //
-        //     var body = {
-        //         response_type: "in_channel",
-        //         text: data
-        //     };
-        //
-        //     res.send(body);
-        // }
+        if (!error && response.statusCode == 200) {
+            var data = JSON.parse(body);
+            // var first_url = data.response.hits[0].result.url;
 
-        res.send(body);
+            var body = {
+                response_type: "in_channel",
+                text: data
+            };
+
+            res.send(body);
+        } else if(error){
+          res.send(error);
+        } else {
+          res.send(body);
+        }
+
+
     });
 });
 
