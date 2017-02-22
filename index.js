b86b86aa0c6d2b2193546d90077a4c9ffe98ec2c
DaveWexler <dwexler@gmail.com>
2017-02-22 12:24:03 -0500

    fix lat-lon attribute names in xapix call

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const xapix_auth_key = process.env.AUTH_KEY || require('./token.js');
 const baseURL = 'https://xap.ix-io.net/api/v1';
 const airportBaseURL = baseURL + '/my_project_trUN0hXK/airports/';
 const uberBaseURL = baseURL + '/uber/time_estimates';
-const xapxi_headers = {
+const xapix_headers = {
     'Authorization': xapix_auth_key,
     'Accept': 'application/json'
 };
@@ -28,7 +28,7 @@ app.set('port', (process.env.PORT || 9001));
 app.get('/', function(req, res) {
     let options = {
         url: airportBaseURL + _.toUpper('jfk'),
-        headers: xapxi_headers
+        headers: xapix_headers
     };
     rp(options)
         .then(response => {
@@ -47,7 +47,7 @@ app.post('/post', function(req, res) {
     console.log('req body: ', req.body);
     let options = {
         url: airportBaseURL + _.toUpper(req.body.text),
-        headers: xapxi_headers
+        headers: xapix_headers
     };
 
     rp(options)
@@ -68,10 +68,10 @@ function setUpUberReq(airportResp) {
     const airportdata = JSON.parse(airportResp);
     let options = {
         url: uberBaseURL,
-        headers: xapxi_headers,
+        headers: xapix_headers,
         qs: {
-            'filter[lat]': airportdata.airport.latitude,
-            'filter[lon]': airportdata.airport.longitude
+            'filter[lat]': airportdata.airport.position_latitude,
+            'filter[lon]': airportdata.airport.position_longitude
         }
     };
     return rp(options)
